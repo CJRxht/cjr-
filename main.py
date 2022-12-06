f93a788dec92296c2ab6f1db2218833a87970a94
CJRxht <114345705+CJRxht@users.noreply.github.com>
2022-12-06 19:54:57 +0800

    Create main.py

--- main.py
+++ main.py
@@ -46,14 +46,18 @@ def get_weather(region):
     weather_url = "https://free-api.heweather.com/s6/weather/forecast?location={}&key={}".format(location_id, key)
     response = get(weather_url, headers=headers).json()
     # 天气帅达版
-    weather = response['HeWeather6'][0]["daily_forecast"][0]["cond_txt_d"]+'至'+response['HeWeather6'][0]["daily_forecast"][0]["cond_txt_n"]
+    weather = '白天'+response['HeWeather6'][0]["daily_forecast"][0]["cond_txt_d"]+'，'+'傍晚'+response['HeWeather6'][0]["daily_forecast"][0]["cond_txt_n"]
     # 当前温度
-    temp = response['HeWeather6'][0]["daily_forecast"][0]["tmp_max"]+ u"\N{DEGREE SIGN}" + "C"
+    temp = response['HeWeather6'][0]["daily_forecast"][0]["tmp_min"]+ u"\N{DEGREE SIGN}" + "C"+'—'+response['HeWeather6'][0]["daily_forecast"][0]["tmp_max"]+ u"\N{DEGREE SIGN}" + "C"
+    if int(response['HeWeather6'][0]["daily_forecast"][0]["tmp_min"]) <= 20:
+        xigua = "天气变凉啦，多穿点衣服哦~"
+    else:
+        xigua = "今天又是很想你的一天~"
     # 风向
     wind_dir = response['HeWeather6'][0]["daily_forecast"][0]["wind_dir"]
-    return weather, temp, wind_dir
+    return weather, temp, wind_dir, xigua
  
- 
+#--------关注微信公众号：繁星资源，更多资源等你拿----------
 def get_birthday(birthday, year, today):
     birthday_year = birthday.split("-")[0]
     # 判断是否为农历生日
@@ -108,7 +112,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, xigua, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -165,6 +169,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             "note_ch": {
                 "value": note_ch,
                 "color": get_color()
+            },
+            "xigua":{
+                "value": xigua,
+                "color": get_color()
             }
         }
     }
@@ -214,7 +222,7 @@ if __name__ == "__main__":
     users = config["user"]
     # 传入地区获取天气信息
     region = config["region"]
-    weather, temp, wind_dir = get_weather(region)
+    weather, temp, wind_dir,xigua = get_weather(region)
     note_ch = config["note_ch"]
     note_en = config["note_en"]
     if note_ch == "" and note_en == "":
@@ -222,5 +230,5 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, xigua, wind_dir, note_ch, note_en)
     os.system("pause")
